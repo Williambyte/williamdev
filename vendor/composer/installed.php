9c379abee47aa0eec1701d9cd1bb829bab30137a
Williambyte <williaokwach1536@gmail.com>
2020-11-10 00:53:41 +0300

    php installation

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'dd362e318a1159987430adfeec103ed653aa44af',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dd362e318a1159987430adfeec103ed653aa44af',
+    ),
+  ),
+);
